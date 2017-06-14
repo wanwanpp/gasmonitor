ce39988d772f75bc0843307430ae53da0652304b
王萍 <wang.03.25@qq.com>
2017-06-14 21:52:49 +0800

    增加邮件发送服务，并测试

--- src/main/java/com/gasmonitor/service/mail/MailService.java
+++ src/main/java/com/gasmonitor/service/mail/MailService.java
@@ -3,5 +3,7 @@ package com.gasmonitor.service.mail;
 import javax.mail.MessagingException;
 
 public interface MailService {
+
+
 	void send(String to, String subject, String text) throws MessagingException;
 }
