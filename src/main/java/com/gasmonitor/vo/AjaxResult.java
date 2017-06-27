e6902ad458cbe89ca7ce028601883af2281a117a
saplmm <1046439010@qq.com>
2017-06-27 21:23:20 +0800

    1，ajax result 增加restring 2, 租户管理增加button的icon

--- src/main/java/com/gasmonitor/vo/AjaxResult.java
+++ src/main/java/com/gasmonitor/vo/AjaxResult.java
@@ -1,1 +1,88 @@
-package com.gasmonitor.vo;import org.springframework.data.domain.Page;import java.util.List;/** * Created by saplmm on 2017/6/23. */public class AjaxResult<T> {    public static Integer PAGE_SIZE = 2;    private long total;    private int page;    private long totalPage;    private List<T> data;    public AjaxResult() {    }    public AjaxResult(List<T> data) {        this.data = data;        this.setTotal(data.size());        this.setPage(1);        this.setTotalPage(1);    }    public static <M> AjaxResult<M> NewAjaxResult(List<M> t, long total, int page) {        AjaxResult<M> r = new AjaxResult<M>();        r.setData(t);        r.setTotal(total);        r.setPage(page);        return r;    }    public static <M> AjaxResult<M> NewAjaxResult(Page<M> page) {        AjaxResult<M> r = new AjaxResult<M>();        r.setData(page.getContent());        r.setPage(page.getNumber());        r.setTotalPage(page.getTotalPages());        r.setTotal(page.getTotalElements());        return r;    }    @Override    public String toString() {        return "AjaxResult{" +                "total=" + total +                ", page=" + page +                ", data=" + data +                '}';    }    public long getTotal() {        return total;    }    public void setTotal(long total) {        this.total = total;    }    public int getPage() {        return page;    }    public void setPage(int page) {        this.page = page;    }    public List<T> getData() {        return data;    }    public void setData(List<T> data) {        this.data = data;    }    public long getTotalPage() {        return totalPage;    }    public void setTotalPage(long totalPage) {        this.totalPage = totalPage;    }}
+package com.gasmonitor.vo;
+
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+/**
+ * Created by saplmm on 2017/6/23.
+ */
+public class AjaxResult<T> {
+    public static Integer PAGE_SIZE = 2;
+    private long total;
+    private int page;
+    private long totalPage;
+    private List<T> data;
+
+
+    public AjaxResult() {
+    }
+
+    public AjaxResult(List<T> data) {
+        this.data = data;
+        this.setTotal(data.size());
+        this.setPage(1);
+        this.setTotalPage(1);
+    }
+
+    public static <M> AjaxResult<M> NewAjaxResult(List<M> t, long total, int page) {
+        AjaxResult<M> r = new AjaxResult<M>();
+        r.setData(t);
+        r.setTotal(total);
+        r.setPage(page);
+        return r;
+    }
+
+
+    public static <M> AjaxResult<M> NewAjaxResult(Page<M> page) {
+        AjaxResult<M> r = new AjaxResult<M>();
+        r.setData(page.getContent());
+        r.setPage(page.getNumber());
+        r.setTotalPage(page.getTotalPages());
+        r.setTotal(page.getTotalElements());
+        return r;
+    }
+
+    @Override
+    public String toString() {
+        return "AjaxResult{" +
+                "total=" + total +
+                ", page=" + page +
+                ", totalPage=" + totalPage +
+                ", data=" + data +
+                '}';
+    }
+
+    public long getTotal() {
+        return total;
+    }
+
+    public void setTotal(long total) {
+        this.total = total;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public List<T> getData() {
+        return data;
+    }
+
+    public void setData(List<T> data) {
+        this.data = data;
+    }
+
+    public long getTotalPage() {
+        return totalPage;
+    }
+
+    public void setTotalPage(long totalPage) {
+        this.totalPage = totalPage;
+    }
+
+}
