2d4e54f3652a9bb928cb907d3fda0b6699672a53
Pandaroid <Payne.Pandaroid.Wang@foxmail.com>
2017-07-10 18:41:15 +0800

    测试：推送 hazel 测试数据

--- src/main/java/com/gasmonitor/service/middleware/FakeDataProducer.java
+++ src/main/java/com/gasmonitor/service/middleware/FakeDataProducer.java
@@ -1,5 +1,7 @@
 package com.gasmonitor.service.middleware;
 
+import com.gasmonitor.entity.GasEvent;
+import com.gasmonitor.entity.GasHazelcast;
 import com.gasmonitor.service.middleware.api.WsClientPoolApi;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,7 +25,23 @@ public class FakeDataProducer implements CommandLineRunner {
         for (int i = 0; i < 100000; i++) {
             try {
                 Thread.sleep(10000);
-                wsClientPoolApi.sendMonitorData("测试数据...");
+                // wsClientPoolApi.sendMonitorData("测试数据...");
+
+                // Start: 发送 hazel 的测试数据
+                GasHazelcast hazelcastEvent = new GasHazelcast();
+                hazelcastEvent.setTenantId("test tenantId");
+                GasEvent gasEvent = new GasEvent();
+                gasEvent.setHardwareId("test hardwareId");
+                gasEvent.setPointtime(222222);
+                gasEvent.setPressure(33333);
+                gasEvent.setRunning(44444);
+                gasEvent.setStandard(55555);
+                gasEvent.setSummary(66666);
+                gasEvent.setTemperature(77777);
+                hazelcastEvent.setGasEvent(gasEvent);
+                wsClientPoolApi.sendMonitorData(hazelcastEvent);
+                // End  : 发送 hazel 的测试数据
+
             } catch (Exception e) {
             }
         }
