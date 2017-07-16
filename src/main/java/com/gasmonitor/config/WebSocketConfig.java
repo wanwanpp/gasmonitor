908843e7d316e879c2194a13bb8a77258d8bb5a0
saplmm <1046439010@qq.com>
2017-07-17 00:32:03 +0800

    增加web socket 的session

--- src/main/java/com/gasmonitor/config/WebSocketConfig.java
+++ src/main/java/com/gasmonitor/config/WebSocketConfig.java
@@ -11,8 +11,8 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
  * 首先要注入ServerEndpointExporter，这个bean会自动注册使用了@ServerEndpoint注解声明的Websocket endpoint。
  * 要注意，如果使用独立的servlet容器，而不是直接使用springboot的内置容器，就不要注入ServerEndpointExporter，因为它将由容器自己提供和管理。
  */
-@Configuration
-@EnableWebSocketMessageBroker//1
+//@Configuration
+//@EnableWebSocketMessageBroker//1
 public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
     @Override
