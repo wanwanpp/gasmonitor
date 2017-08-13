8cbfe0056e9c9dab60e3ebb0eafc22890c2a5892
Pandaroid <Payne.Pandaroid.Wang@foxmail.com>
2017-08-13 19:10:58 +0800

    index.js 中增加对 webStorageCache 和 monitorDataCacheManager 的调用。在 oneSocket 监听 GAS_EVENT 的 handler 中，调用 monitorDataCacheManager 的 addJsonMonitorData2Cache 进行增量更新

--- src/main/resources/public/js/app/index.js
+++ src/main/resources/public/js/app/index.js
@@ -9,10 +9,13 @@
         layui.tools.indexLeftNavTopTab();
         // 处理 oneSocket
         var $ = layui.jquery
-            , webStorageCache = layui.webStorageCache;
+            , webStorageCache = layui.webStorageCache
+            , monitorDataCacheManager = webStorageCache.monitorDataCacheManager;
         $(function() {
+            // Start: oneSocket 相关代码
             var oneSocket = layui.oneSocket;
             oneSocket(SockJS, Stomp);
+            // 1. 设置 oneSocket 的 handler ，在其中进行 webStorageCache 的增量更新
             oneSocket.setHandler(oneSocket.Event.GAS_EVENT, handler_gatherGasEventMonitorData);
             // 定义 oneSocket 的 handler
             function handler_gatherGasEventMonitorData(oneSocketMsgBody) {
@@ -20,16 +23,13 @@
                 console.log('[index.js handler_gatherGasEventMonitorData]oneSocketMsgBody: ');
                 console.log(oneSocketMsgBody);
                 // 1. gasEvent
-                var jsonObj_monitorData = JSON.parse(oneSocketMsgBody)
-                    , jsonObj_gasEvent = jsonObj_monitorData.gasEvent;
-                // 2. jsonObj_gasEvent: {"hardwareId":"t21s1d1","temperature":1212.0,"pressure":781.0,"standard":9061.0
-                // ,"running":758.0,"summary":1929391.0,"surplus":2.1421421E7,"analog1":2.1421421E7,"analog2":2.1421421E7
-                // ,"analog3":2.1421421E7,"analog4":2.1421421E7,"switch1":21421421,"switch2":21421421,"switch3":21421421
-                // ,"switch4":21421421,"ac220":21421421,"battery":21421421,"solar":21421421,"pointtime":1501833236607}
-                // ,"gaojing":true,"msg":"温度太高"}
-
+                var jsonObj_monitorData = JSON.parse(oneSocketMsgBody);
+                monitorDataCacheManager.addJsonMonitorData2Cache(jsonObj_monitorData);
 
             }
+            // 2. 进行一次今日的全量拉取，拉取后 add 到 webStorageCache
+
+            // End  : oneSocket 相关代码
         });
     });
 })();
