b4b243988360416c874bcbd1d5bb7d1e90c1067b
saplmm <1046439010@qq.com>
2017-08-15 01:02:42 +0800

    编译报错

--- src/main/java/com/gasmonitor/controller/test/TestController.java
+++ src/main/java/com/gasmonitor/controller/test/TestController.java
@@ -16,8 +16,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.thymeleaf.context.WebEngineContext;
 
 import javax.servlet.http.HttpSession;
 import java.security.Principal;
