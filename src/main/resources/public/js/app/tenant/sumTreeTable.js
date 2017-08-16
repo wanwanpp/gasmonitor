2ee1f1437b75d5f4651459208ef64196dd8abf7c
Pandaroid <pandaroid@github.com>
2017-08-16 23:58:04 +0800

    优化：对于第二级 devicesArr （有回调），在二级 devicesArr 为空时，执行回调；否则，是一级 devicesArr ，为空则直接 process 下一个 site

--- src/main/resources/public/js/app/tenant/sumTreeTable.js
+++ src/main/resources/public/js/app/tenant/sumTreeTable.js
@@ -54,7 +54,12 @@
                             function processSumTreeTableDevicesArr(devices_arr, index_devices_arr, callback_processSumTreeTableDevicesArr) {
                                 if(!devices_arr || !devices_arr.length || devices_arr.length < 1) {
                                     console.log('[processSumTreeTableDevicesArr]devices_arr 为空，不继续处理，继续处理下一个 site');
-                                    processSumTreeTableSite(data_siteArr, index_data_siteArr + 1);
+                                    if(callback_processSumTreeTableDevicesArr
+                                        && callback_processSumTreeTableDevicesArr instanceof Function) {
+                                        callback_processSumTreeTableDevicesArr();
+                                    } else {
+                                        processSumTreeTableSite(data_siteArr, index_data_siteArr + 1);
+                                    }
                                     return ;
                                 }
                                 var len_devices_arr = devices_arr.length;
