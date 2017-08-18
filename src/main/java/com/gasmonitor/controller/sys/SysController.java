0ef70d7ac6279efc611a8567fa2eeebe72b83e44
saplmm <1046439010@qq.com>
2017-08-18 22:53:35 +0800

    首页增加信息

--- src/main/java/com/gasmonitor/controller/sys/SysController.java
+++ src/main/java/com/gasmonitor/controller/sys/SysController.java
@@ -1,1 +1,34 @@
-package com.gasmonitor.controller.sys;import org.springframework.stereotype.Controller;import org.springframework.web.bind.annotation.RequestMapping;/** * Created by saplmm on 2017/6/23. */@Controller@RequestMapping(value = "sys")public class SysController {    @RequestMapping(value = "setting")    public String setting() {        return "sys/setting";    }}
+package com.gasmonitor.controller.sys;
+
+import com.gasmonitor.dao.DeviceRepository;
+import com.gasmonitor.dao.TenantRepository;
+import com.gasmonitor.vo.SysInfoVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by saplmm on 2017/6/23.
+ */
+@Controller
+@RequestMapping(value = "sys")
+public class SysController {
+
+    @Autowired
+    private TenantRepository tenantRepository;
+
+    @Autowired
+    private DeviceRepository deviceRepository;
+
+
+    @RequestMapping(value = "setting")
+    public String setting(ModelMap modelMap) {
+        SysInfoVo vo = new SysInfoVo();
+        vo.setDeviceCount(deviceRepository.count());
+        vo.setTenantCount(tenantRepository.count());
+        vo.setMsg("暂无警告");
+        modelMap.addAttribute("vo", vo);
+        return "sys/setting";
+    }
+}
--- src/main/java/com/gasmonitor/vo/SysInfoVo.java
+++ src/main/java/com/gasmonitor/vo/SysInfoVo.java
@@ -1,1 +1,34 @@
-package com.gasmonitor.vo;/** * Created by saplmm on 2017/8/13. */public class SysInfoVo {}
+package com.gasmonitor.vo;
+
+/**
+ * Created by saplmm on 2017/8/13.
+ */
+public class SysInfoVo {
+    private Long tenantCount;
+    private Long deviceCount;
+    private String msg;
+
+    public Long getTenantCount() {
+        return tenantCount;
+    }
+
+    public void setTenantCount(Long tenantCount) {
+        this.tenantCount = tenantCount;
+    }
+
+    public Long getDeviceCount() {
+        return deviceCount;
+    }
+
+    public void setDeviceCount(Long deviceCount) {
+        this.deviceCount = deviceCount;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
