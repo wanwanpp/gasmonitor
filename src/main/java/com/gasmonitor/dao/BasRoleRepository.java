eb735490b74a27cba6603ac6179c5f0b2b54632e
saplmm <1046439010@qq.com>
2017-08-18 23:57:37 +0800

    基础数据

---
+++ src/main/java/com/gasmonitor/dao/BasRoleRepository.java
@@ -0,0 +1,1 @@
+package com.gasmonitor.dao;import com.gasmonitor.entity.BasRole;import org.springframework.data.jpa.repository.JpaRepository;/** * Created by saplmm on 2017/8/18. */public interface BasRoleRepository extends JpaRepository<BasRole, Long> {}
